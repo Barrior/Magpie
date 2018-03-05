5d1e6b1b0f4e83f9d0405af2a5ca6ac07017d419
Barrior <barrior@qq.com>
2018-03-05 15:45:01 +0800

    adjust to 2 spaces indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "root": true,
-    "parser": "babel-eslint",
-    "env": {
-        "es6": true,
-        "browser": true,
-        "node": true
-    },
-    "extends": ["standard"]
+  "root": true,
+  "parser": "babel-eslint",
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true
+  },
+  "extends": ["standard"]
 }
