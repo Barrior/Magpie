430ae5679579308fc9577ddfdd47bad63941a2dd
Barrior <barrior@qq.com>
2018-03-06 15:09:41 +0800

    improve types.js

--- test/helpers/types.js
+++ test/helpers/types.js
@@ -1,10 +1,11 @@
-export default [
+export const types = [
   {type: '[object Function]', value: () => {}},
   {type: '[object Object]', value: {}},
   {type: '[object Array]', value: []},
-  {type: '[object String]', value: 'string'},
+  {type: '[object String]', value: ''},
   {type: '[object Boolean]', value: true},
   {type: '[object Boolean]', value: false},
+  {type: '[object Number]', value: -1},
   {type: '[object Number]', value: 0},
   {type: '[object Number]', value: 1},
   {type: '[object Null]', value: null},
@@ -12,6 +13,18 @@ export default [
   {type: '[object Symbol]', value: Symbol('object Symbol')}
 ]
 
-export const typeChecking = (value) => {
-  return toString.call(value)
+/* Types checkout */
+// Symbol type should throw an error,
+// other types(excluded Number and String) should return false.
+export const typesCheckout = (method, t) => {
+  types.forEach((type) => {
+    const {value, type: typeString} = type
+    if (typeString === '[object Symbol]') {
+      t.throws(() => {
+        method(value)
+      })
+    } else if (typeString !== '[object Number]' || typeString !== '[object String]') {
+      t.false(method(value))
+    }
+  })
 }
