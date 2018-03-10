048d80b6aded664f88b80f8dc772c64027e23e18
Barrior <barrior@qq.com>
2018-03-10 10:46:33 +0800

    postcode checkout

--- test/postcodeCN.js
+++ test/postcodeCN.js
@@ -5,9 +5,18 @@ test('isPassword', (t) => {
   /* Normal rules checkout */
   t.true(isPostcodeCN(100000))
   t.true(isPostcodeCN(123456))
-  t.true(isPostcodeCN('012345'))
   t.false(isPostcodeCN(12345))
   t.false(isPostcodeCN(1234567))
 
+  t.true(isPostcodeCN('012345'))
   t.true(isPostcodeCN('123456'))
+
+  for (let i = 0; i < 10; i++) {
+    const str = '1'.repeat(i)
+    if (i === 6) {
+      t.true(isPostcodeCN(str))
+    } else {
+      t.false(isPostcodeCN(str))
+    }
+  }
 })
