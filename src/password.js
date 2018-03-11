5129e1be7c0b1195fa6c42ea3373021c37ef503f
Barrior <barrior@qq.com>
2018-03-11 10:37:26 +0800

    save work

--- src/password.js
+++ src/password.js
@@ -1,23 +1,33 @@
-/* eslint-disable no-useless-escape */
-
 /**
  * Spec:
- * 1、只能包含英文字符，数字。
- * 2、空字符只包含英文空格，不包含制表符等空字符。
- * 3、长度至少为 1。
+ * 1. Can only contain English characters, numbers and spaces.
+ * 2. At least one character.
  */
-export const $password = /^[\w\040`~!@#$%^&*()\-+=\[\]{};:'"\\|,.<>/?]+$/
+
+/**
+ * 规格：
+ * 1、只能包含英文字符，数字，空格。
+ * 2、长度至少为 1。
+ */
+export const $password = /^[\w\040`~!@#$%^&*()\-+=[\]{};:'"\\|,.<>/?]+$/
 export const isPassword = (value) => {
   return $password.test(value)
 }
 
 /**
  * Spec:
+ * 1. Not English characters, numbers and spaces.
+ * 2. Matches blank characters that except spaces, such as tabs.
+ * 3. Replace all characters that match the rule with an empty string.
+ */
+
+/**
+ * 规格：
  * 1、不是英文字符，数字。
  * 2、匹配除空格外的其他空字符，如制表符。
  * 3、替换所有符合规则的字符为空字符串。
  */
-export const $noPassword = /[^\w\040`~!@#$%^&*()\-+=\[\]{};:'"\\|,.<>/?]*/g
+export const $noPassword = /[^\w\040`~!@#$%^&*()\-+=[\]{};:'"\\|,.<>/?]*/g
 export const formatPassword = (value) => {
   return value.replace($noPassword, '')
 }
--- src/qq.js
+++ src/qq.js
@@ -9,10 +9,16 @@
 
 /**
  * Spec:
- * 1. Required 5~12 characters(preset max length of 12), only Number type.
- * 2. The first character can not be 0.
- *
- * 要求 5~12 位（先保留到 12 位），不能以 0 开头，只能是数字
+ * 1. Required 5~12 characters, preset max length of 12.
+ * 2. Can only be numbers.
+ * 3. The first character can not be 0.
+ */
+
+/**
+ * 规格：
+ * 1、要求 5~12 位，先保留到 12 位。
+ * 2、只能是数字。
+ * 3、不能以 0 开头。
  */
 export const $qq = /^[1-9]\d{4,11}$/
 export const isQQ = (value) => {
