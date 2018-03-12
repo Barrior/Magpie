34f6591c54b44071ce8a3fda4ea000465dc4e736
Barrior <barrior@qq.com>
2018-03-12 21:08:54 +0800

    save work

--- src/domain.js
+++ src/domain.js
@@ -1,1 +1,8 @@
-export const $domain = /^[a-z\d](([a-z\d-]*)?[a-z\d])?(\.[a-z\d]{2,6})*$/i
+/**
+ * 规格：
+ * 1、不区分大小写。
+ */
+export const $domain = /^[a-z\d](([a-z\d-]*)?[a-z\d])?(\.[a-z\d][a-z\d-]*[a-z\d]?)?\.[a-z]{2,6}$/i
+export const isDomain = (value) => {
+  return $domain.test(value)
+}
---
+++ test/domain.js
@@ -0,0 +1,21 @@
+import test from 'ava'
+import {isDomain} from '../src/domain'
+
+test('isPassword', (t) => {
+  /* Normal rules checkout */
+  t.true(isDomain('a.com'))
+  t.true(isDomain('a.a.com'))
+  t.true(isDomain('abc.abc.org'))
+  t.true(isDomain('abc-12.de-34-56.com'))
+  t.true(isDomain('abc-12.de-34-56.890-90.com'))
+
+  t.false(isDomain('a.com-'))
+  t.false(isDomain('a.co-m'))
+  t.false(isDomain('-a.com'))
+  t.false(isDomain('a-.com'))
+  t.false(isDomain('ab.-a.com'))
+  t.false(isDomain('ab.a-.com'))
+
+  t.false(isDomain('-ab.a.com'))
+  t.false(isDomain('ab-.a.com'))
+})
