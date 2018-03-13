740faaf4373f393bacd99c4ac14daa4fa1819bdb
Barrior <barrior@qq.com>
2018-03-13 21:58:15 +0800

    save work

--- src/domain.js
+++ src/domain.js
@@ -1,8 +1,13 @@
 /**
  * 规格：
- * 1、不区分大小写。
+ * https://en.wikipedia.org/wiki/Domain_name
+ *
+ * 1、域名由英文字母（不区分大小写）、数字、"-"（中横线）构成。
+ * 2、每级域名首尾只能为英文字母，数字。
+ * 3、每一级域名最小长度为 1 个字符，最大长度为 63 个字符，且域名总长度不能超过 253 个字符。
+ * 4、预设顶级域名只能为英文字母，且最小长度为 2。
  */
-export const $domain = /^[a-z\d](([a-z\d-]*)?[a-z\d])?(\.[a-z\d][a-z\d-]*[a-z\d]?)?\.[a-z]{2,6}$/i
+export const $domain = /^([a-z\d](([a-z\d-]*)?[a-z\d])?(\.[a-z\d](([a-z\d-]*)?[a-z\d])?)?\.[a-z]{2,63}){4,253}$/i
 export const isDomain = (value) => {
   return $domain.test(value)
 }
--- test/domain.js
+++ test/domain.js
@@ -3,12 +3,16 @@ import {isDomain} from '../src/domain'
 
 test('isDomain', (t) => {
   /* Normal rules checkout */
+  // Correct rules checkout
   t.true(isDomain('a.com'))
   t.true(isDomain('a.a.com'))
-  t.true(isDomain('abc.abc.org'))
+  t.true(isDomain('abc.abc.com'))
   t.true(isDomain('abc-12.de-34-56.com'))
   t.true(isDomain('abc-12.de-34-56.890-90.com'))
 
+  // Errors rules checkout
+  t.false(isDomain('com.'))
+  t.false(isDomain('.com'))
   t.false(isDomain('a.com-'))
   t.false(isDomain('a.co-m'))
   t.false(isDomain('-a.com'))
@@ -18,4 +22,18 @@ test('isDomain', (t) => {
 
   t.false(isDomain('-ab.a.com'))
   t.false(isDomain('ab-.a.com'))
+
+  /* Specific errors checkout */
+  // Top-level domains that at least 2 characters.
+  t.false(isDomain('a.c'))
+  // Top-level domains can only be letters.
+  t.false(isDomain('a.c2'))
+  t.false(isDomain('a.c-'))
+  t.false(isDomain('a.c-m'))
+
+  // Length checkout
+  // Each level domains that max character of 63.
+  t.false(isDomain(`${'a'.repeat(64)}.com`))
+  // Domains max length can not more than 253.
+  t.false(isDomain(`${'a'.repeat(250)}.com`))
 })
